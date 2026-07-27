8061f940e6775aa62c5220dda20e465a97b2beda
saumyachowdhury <57725873+saumyachowdhury@users.noreply.github.com>
2026-07-27 20:28:23 +0530

    Bulk sync: 4 submissions

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-2.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int n=nums.length;
+        int[] ans= new int[n*2];
+        // HashMap<Integer,Integer> map=new HashMap<>();
+        // for(int i=0;i<n;i++){
+        //     map.put(i,nums[i]);
+        //     ans[i]=map.get(i);
+        //     ans[i+n]=map.get(i);
+        // }
+
+        for(int i=0;i<n;i++){
+            ans[i]=nums[i];
+            ans[i+n]=nums[i];     
+             }
+        
+        return ans;  
+
+    }
+}
